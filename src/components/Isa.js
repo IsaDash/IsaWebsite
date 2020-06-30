8b45411ba02b3c294f0e4f2c67dc3c0c39729ecb
isadash <anchald2@illinois.edu>
2020-06-30 00:59:07 -0700

    new layout, color change, about section

--- src/components/Isa.js
+++ src/components/Isa.js
@@ -16,33 +16,54 @@ function Isa() {
                 <h1 className="header-text">
                     <Scramble
                         autoStart
-                        text="Hey, I'm Isa."
+                        text="Hey, I'm Isa and I'm an engineer."
                         steps={[
                             {
-                                roll: 20,
+                                roll: 30,
                                 action: '+',
-                                type: 'random',
-                                speed: 'slow'
+                                type: 'forward',
+                                speed: 'medium'
                             },
                             {
-                                roll: 20,
+                                roll: 30,
                                 action: '-',
                                 type: 'forward',
-                                speed: 'slowest'
+                                speed: 'medium'
 
                             },
                         ]}
                     />
 
                 </h1>
-                <p className="p-text">
+                <h1 className="p-text">
+                    Welcome to my website!
+                    
+                </h1>
+                <p className="navigation-text">
+                    <a className="navigation-text" href="https://github.com/IsaDash" target="_blank">
+                        1. about
+                    </a>
+                    <a className="navigation-text" href="https://github.com/IsaDash" target="_blank">
+                        2. skills
+                    </a>
+                    <a className="navigation-text" href="https://github.com/IsaDash" target="_blank">
+                        3. featured work
+                    </a>
+                    <a className="navigation-text" href="https://github.com/IsaDash" target="_blank">
+                        4. other
+                    </a>
+                </p>
+
+                {/* <p className="p-text">
                     <p>
                     I'm a Math and CS major at the University of Illinois at Urbana-Champaign class of 2022;
                 </p>
                 I'm a software engineering intern at JP Morgan Chase as of July 2020;
 
 
-            </p>
+            </p> */}
+
+            
             </div>
             </div>
         </header>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,12 +2,33 @@ import React from 'react';
 import './Projects.css';
 import Image from './Image'
 
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
+
+
 function Projects() {
     return (
         <header className="projects">
-            <div >
+            <div data-aos="fade-left" >
                 <h1 className="project-container header">
-                    Featured Projects
+                    1. about
+                    <p className="about-text">
+                        Hello! My name is Isa Dash and I'm from San Jose, CA.
+                        I am a Junior majoring in Math and Computer Science at the <span className="highlight"> University of Illinois at Urbana-Champaign.</span>
+                        
+                    </p>
+                    <p className="about-text">
+                    Currently, I am a summer Software Engineering Intern at 
+                     <span className="highlight"> JP Morgan Chase</span>, building software that helps non-profits.
+                      
+                    </p>
+                    <p className="about-text" color="red">
+                    I am constantly exploring topics new to me like fullstack development, machine learning, data science, web development (which is how this website was brought to you!), and more.
+                    I am gathering the tools to make a positive difference in the world with technology :)
+
+                    </p>
+                    
                 </h1>
 
             </div>
